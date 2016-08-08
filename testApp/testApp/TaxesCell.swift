4098d7be426c12c50a0e916f0ee1e0f6360fc5cb
Zbranevich, Andrey <a.zbranevich@itransition.com>
2016-08-08 13:50:22 +0300

    add taxes page

---
+++ testApp/testApp/TaxesCell.swift
@@ -0,0 +1,21 @@
+//
+//  taxesCell.swift
+//  testApp
+//
+//  Created by Zbranevich, Andrey on 8/8/16.
+//  Copyright © 2016 Zbranevich, Andrey. All rights reserved.
+//
+
+import UIKit
+
+class taxesCell: BaseTableCell {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ testApp/testApp/TaxesController.swift
@@ -0,0 +1,13 @@
+//
+//  TaxesController.swift
+//  testApp
+//
+//  Created by Zbranevich, Andrey on 8/8/16.
+//  Copyright © 2016 Zbranevich, Andrey. All rights reserved.
+//
+
+import UIKit
+
+class TaxesController: BaseTable {
+
+}
